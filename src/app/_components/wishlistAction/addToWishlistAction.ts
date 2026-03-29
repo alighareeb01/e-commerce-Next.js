7158234373db7495aeb4707ba005d71fcd45d706
alighareeb01 <ghareeb874@gmail.com>
2026-03-29 02:50:52 +0200

    added wishlist feature

---
+++ src/app/_components/wishlistAction/addToWishlistAction.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import getMyToken from "@/app/utils/getMyToken";
+
+export default async function addToWishlistAction(productId: string) {
+  const token = await getMyToken();
+  const res = await fetch(
+    "https://nti-e-commerce-backend-project.vercel.app/api/v1/wishlist",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        product: productId,
+      }),
+    },
+  );
+  const data = await res.json();
+  console.log(data);
+}
